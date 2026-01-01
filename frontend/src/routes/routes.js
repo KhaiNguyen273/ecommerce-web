db7847dc5c5014fc504e9d453635544ec7d3ec56
KhaiNguyen273 <luuphungkhainguyen2732005@gmail.com>
2026-01-01 19:26:52 +0700

    Add frontend folder as normal folder

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,10 @@
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login, layout: null },
+];
+
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
